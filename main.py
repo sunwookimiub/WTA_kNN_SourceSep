eaabd402f183d845650ef7d2eb55e5a717cf6f32
kimsunw-admin <kimsunw-admin@149-160-134-127.dhcp-bl.indiana.edu>
2019-04-19 09:53:43 -0400

    Added line to try showing MPL on server

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from argparse import ArgumentParser
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import _pickle as pickle
 import os
@@ -73,7 +75,8 @@ def main():
     
     if args.is_debug:
         # debug_ind_noise_snr(data, args, mel_Fs, stft_Fs, model_nm) # kNN
-        debug_wta_snr(args, mel_Fs, stft_Fs, Ls) # WTA
+        # debug_wta_snr(args, mel_Fs, stft_Fs, Ls) # WTA
+        print ("Nothing here")
 
     else:
         print ("Running {}...".format(model_nm))
